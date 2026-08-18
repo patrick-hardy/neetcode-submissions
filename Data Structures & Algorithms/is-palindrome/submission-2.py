dc59e075a229deda7ccfd629cae473e34ee374b6
patrick-hardy <165184928+patrick-hardy@users.noreply.github.com>
2026-08-18 00:38:17 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        s = "".join(x for x in s if x.isalnum()).lower()
+        revString = s[::-1]
+        revString = "".join(c for c in revString if c.isalnum()).lower()
+        return s == revString
